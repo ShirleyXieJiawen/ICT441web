f089da41a9875219938decfa4548a0aed0b97792
ShirleyXieJiawen <127078480+ShirleyXieJiawen@users.noreply.github.com>
2023-05-04 11:34:59 +0800

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+function Getstart(){ 
+    var input=prompt("Please choose your content 'student'or'parent'")
+        if (input == "student"){
+         window.location.href="https://shirleyxiejiawen.github.io/ICT441web/services.html" 
+        }
+        
+        else if (input == "parent")
+        {
+            window.location.href="https://shirleyxiejiawen.github.io/ICT441web/" 
+        }
+    }
